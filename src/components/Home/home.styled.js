b24100d050226714a33be7a60607734a2a6c5898
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-07-27 14:08:07 +0300

    Fix view

--- src/components/Home/home.styled.js
+++ src/components/Home/home.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 export const HeaderTitle = styled.h1`
   padding-top: 20px;
   padding-bottom: 20px;
+  color:  #blue;
 `;
 
 export const ListMovies = styled.ul`
